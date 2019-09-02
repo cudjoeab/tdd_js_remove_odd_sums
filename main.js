f830c8da78df8aa0d38602810440d06997c2bb4d
cudjoeab <cudjoeab@gmail.com>
2019-09-01 22:20:39 -0400

    function removes the list item whos sum is an odd number

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-function removeOddSum() {
+function removeOddSum(numbersList) {
+    numbersList.forEach((element, index)=> {
+        if ((element.a + element.b) % 2 != 0) {
+            numbersList.splice(index, 1);
+        }    
+    })
+    return numbersList;     
 }
 
 module.exports = removeOddSum;
